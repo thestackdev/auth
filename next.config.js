eb95d388226e44d6d7f22c40528e2a8048e195eb
thestackdev <thestackdev@fullstacklab.org>
2023-01-16 00:06:31 +0530

    fix CORS

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3001",
+            value: "*",
           },
           { key: "Access-Control-Allow-Methods", value: "GET,POST" },
           { key: "Access-Control-Allow-Headers", value: headers.join(", ") },
