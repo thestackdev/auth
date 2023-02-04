02bcfdca273defe775013d382143c3a870de1a26
thestackdev <thestackdev@fullstacklab.org>
2023-02-04 10:13:51 +0530

    Add Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,34 +1,35 @@
 const headers = [
-  "Accept",
-  "Accept-Version",
-  "Content-Length",
-  "Content-MD5",
-  "Content-Type",
-  "Date",
-  "X-Api-Version",
-  "X-CSRF-Token",
-  "X-Requested-With",
-];
+  'Accept',
+  'Accept-Version',
+  'Content-Length',
+  'Content-MD5',
+  'Content-Type',
+  'Date',
+  'X-Api-Version',
+  'X-CSRF-Token',
+  'X-Requested-With',
+]
 
 module.exports = {
   reactStrictMode: true,
+  output: 'standalone',
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
           },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
-          { key: "Access-Control-Allow-Headers", value: headers.join(", ") },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST' },
+          { key: 'Access-Control-Allow-Headers', value: headers.join(', ') },
         ],
       },
-    ];
+    ]
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+}
