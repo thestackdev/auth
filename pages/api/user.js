1e33ffc590b188cd1bcbcfe062c558a10cd056a3
thestackdev <thestackdev@fullstacklab.org>
2023-02-12 16:19:58 +0530

    Add Settings page

---
+++ pages/api/user.js
@@ -0,0 +1,30 @@
+// import { getToken } from 'next-auth/jwt'
+import clientPromise from 'lib/mongodb'
+import { ObjectId } from 'mongodb'
+
+export default async function (req, res) {
+  const client = await clientPromise
+  const usersCollection = client.db('auth').collection('users')
+
+  // const token = await getToken({ req })
+
+  // if (!token) {
+  //   res.status(401).json({ message: 'Unauthorized' })
+  //   return
+  // }
+
+  switch (req.method) {
+    case 'POST':
+      const { name } = req.body
+      await usersCollection.updateOne(
+        { _id: new ObjectId('63dbe27add437f32d1062e72') },
+        { $set: { name } }
+      )
+
+      res.status(201).send('ok')
+      break
+    default:
+      res.status(400).json({ message: 'Bad request' })
+      break
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,32 @@
-import { signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 export default function Home() {
-  const { data: session } = useSession();
+  const { data: session } = useSession()
 
   return (
     <div className="flex flex-col items-center justify-center h-screen w-full gap-4">
       <span className="text-lg text-gray-900 md:text-4xl dark:text-white">
-        You are logged in as{" "}
+        You are logged in as{' '}
         {session.user.name ? session.user.name : session.user.email}
       </span>
-      <button
-        type="button"
-        className="text-white w-fit bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        onClick={signOut}
-      >
-        Logout
-      </button>
+      <div className="flex gap-2">
+        <Link
+          href="/settings"
+          className="text-white w-fit bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Settings
+        </Link>
+        <button
+          type="button"
+          className="text-white w-fit bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
+          onClick={signOut}
+        >
+          Logout
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-Home.auth = true;
+Home.auth = true
